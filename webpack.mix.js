231b0cc52e39ea51db926097f68f2f69fedf1494
Gustavo Siqueira <gustavo@id360.com.br>
2020-02-12 18:22:39 -0300

    'teste'

---
+++ webpack.mix.js
@@ -0,0 +1,24 @@
+const mix = require('laravel-mix');
+const LiveReloadPlugin = require('webpack-livereload-plugin');
+
+mix
+    .setPublicPath('public')
+    .webpackConfig({
+        plugins: [
+            new LiveReloadPlugin(),
+        ]
+    })
+    .js('resources/js/main.js', 'js')
+    .sass('resources/scss/styles.scss', 'css',{
+        includePaths: ['node_modules']
+    })
+    .options({
+        processCssUrls: false
+     })
+    .copy('resources/index.html', 'public/index.html')
+    .copy('resources/chamadas.html', 'public/chamadas.html')
+    .copy('resources/classificacao.html', 'public/classificacao.html')
+    .copy('resources/resultados.html', 'public/resultados.html')
+    .copyDirectory('resources/images', 'public/images')
+    .copyDirectory('resources/icomoon/fonts', 'public/fonts');
+ 
